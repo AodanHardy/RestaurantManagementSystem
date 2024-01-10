053b85539b97ddcc60e0836de40a862df56d52bd
Aodan <hardy-a4@ulster.ac.uk>
2024-01-10 09:13:20 +0000

    added DBConn class and dbConstants

---
+++ src/main/java/org/example/Constants/DatabaseConstants.java
@@ -0,0 +1,19 @@
+package org.example.Constants;
+
+/**
+ * The type Database constants.
+ */
+public class DatabaseConstants {
+    /**
+     * The constant URL.
+     */
+    public static final String URL ="jdbc:mysql://localhost:3306/your_database";
+    /**
+     * The constant USERNAME.
+     */
+    public static final String USERNAME  = "your_username";
+    /**
+     * The constant PASSWORD.
+     */
+    public static final String PASSWORD = "your_password";
+}
